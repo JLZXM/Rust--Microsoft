85ac3bb86e008fdf72bb67fe09cd9359ba5b1172
芳芳的猫耳朵 <1264290724@qq.com>
2023-02-12 15:09:45 +0800

    add change

--- half-hour-learning-rust/src/main.rs
+++ half-hour-learning-rust/src/main.rs
@@ -1,5 +1,8 @@
 // This prints "in", then "out"
 fn main() {
+    use std::cmp::min;
+    let least = min(7,1);
+    println!("least = {}", least);
     let x = "out";
     {
         // this is a different `x`
